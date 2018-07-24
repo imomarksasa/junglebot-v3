fe23351241e101b919f2e9a3070739d68c7634ea
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-24 21:54:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ j = 1;
             j = -1;    
       }    
        i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+        client.user.setGame(setGame[i],`https://www.twitch.tv/S-F`);    
 }, ms);    
     
 });
