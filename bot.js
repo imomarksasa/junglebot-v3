631ef433df8d1e06a7a267977bb2bb937825b046
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-24 21:37:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
  const prefix = ">";
-client.on('ready', () => {
-      client.user.setActivity(`>help | ${client.users.size} Users!`,`>inv | By Prince`, { type: 'WATCHING' })
-
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = [`>help | Users: ${client.users.size} `,`>inv | By Jungle TeaM`];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+}, ms);    
+    
 });
 
 client.on("message", message => {
