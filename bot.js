2b22948e2694971818bf2435cd2d7322e8a019ae
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-24 22:38:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,18 @@ client.on('message', message => {
 });
 
 
-
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>bcall')){
+ if (message.author.id !== '337309575864647680') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '337309575864647680') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 	client.on('message', message => {
                                 if(!message.channel.guild) return;
