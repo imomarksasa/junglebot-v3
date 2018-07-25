c9a7cffde22a48a0854b532443b7b5bb253da43f
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-25 09:55:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
  const prefix = ">";
-client.on('ready', () => {
-      client.user.setActivity(`>help | >inv + ${client.users.size} Users!`, { type: 'WATCHING' })
-
+client.on('im hi lol eread lw wow hii ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['>help | >inv',`${client.users.size} Users!`,'By : Jungle TeaM'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`https://www.twitch.tv/S-F`);    
+}, ms);    
+    
 });
 
 client.on("message", message => {
