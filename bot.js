f59077b63e61b526fea5386bdcaa10166f9b7712
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-26 14:36:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  const prefix = ">";
 client.on('im hi lol eread lw wow hii ready', function(){    
     var ms = 40000 ;    
-    var setGame = ['>help | >inv','By : Jungle TeaM'];    
+    var setGame = ['>help | >inv By : Jungle TeaM'];    
     var i = -1;    
     var j = 0;    
     setInterval(function (){    
