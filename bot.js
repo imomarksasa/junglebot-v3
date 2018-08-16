b98141614b45152ef4532eb987f90bff8f60d8fa
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-08-16 11:46:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,6 +101,35 @@ var secreT = [
     }
 });
 
+client.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(0);
+    let prefix = '>';
+    let xp = require("./xp.json");
+    
+if(cmd === `${prefix}level`) {
+if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+   };
+ }
+   let curxp = xp[message.author.id].xp;
+   let curlvl = xp[message.author.id].level;
+   let nxtLvlXp = curlvl * 300;
+   let difference = nxtLvlXp - curxp;
+ 
+   let lvlEmbed = new Discord.RichEmbed()
+   .setAuthor(message.author.username)
+   .setColor(red)
+   .addField("Level", curlvl, true)
+   .addField("XP", curxp, true)
+   .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+ 
+   message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
+}
+});
 
 client.on('message', message => {
      if(message.content.startsWith(prefix + "clear")) {
